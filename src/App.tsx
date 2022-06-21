f020d6dfd3682002c4d01f556b36d49095179888
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-06-20 20:30:33 -0400

    feat: added pages for the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import Navbar from "./components/Navbar";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import Homepage from "./components/Homepage";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
 import "./App.css";
 
 const App = () => {
@@ -9,6 +14,22 @@ const App = () => {
       <div className="App">
         <div className="navbar">
           <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/exchanges" element={<Exchanges />} />
+                <Route
+                  path="/cryptocurrencies"
+                  element={<Cryptocurrencies />}
+                />
+                <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                <Route path="/news" element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
         </div>
       </div>
     </>
