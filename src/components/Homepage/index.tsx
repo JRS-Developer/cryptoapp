df69fb87ad12bc38fa462cd404b5733349827cf8
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-06-24 15:10:59 -0400

    feat: updated Homepage to show stats of cryptos

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,5 +1,60 @@
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
+
+const { Title } = Typography;
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const { data } = useGetCryptosQuery();
+  const stats = data?.data?.stats;
+  const coins = data?.data?.coins;
+
+  const prettyNumber = (num?: number | string): string => {
+    if (typeof num === "undefined") return "";
+
+    return millify(Number(num));
+  };
+
+  return (
+    <>
+      <Title className="heading" level={2}>
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={prettyNumber(stats?.total)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={prettyNumber(stats?.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market Cap"
+            value={prettyNumber(stats?.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total 24h Volume"
+            value={prettyNumber(stats?.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Markets"
+            value={prettyNumber(stats?.totalMarkets)}
+          />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Homepage;
