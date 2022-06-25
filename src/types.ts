2771275cbeb6b113ddad277d872db5e7f97c316d
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-06-25 19:22:19 -0400

    feat: updated types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,40 @@
-export interface GetCryptoResponse {
+export type CoinId = string | number;
+
+export type TimePeriod =
+  | "1h"
+  | "3h"
+  | "12h"
+  | "24h"
+  | "7d"
+  | "30d"
+  | "3m"
+  | "1y"
+  | "3y"
+  | "5y";
+
+export interface GetCryptosResponse {
   data: {
     stats: Stats;
     coins: Coin[];
   };
+}
+
+export interface GetCryptoDetailsResponse {
+  data: {
+    coin: CoinDetail;
+  };
+}
+
+export interface GetCryptoHistoryResponse {
+  data: {
+    change: string;
+    history: CoinHistory[];
+  };
+}
+
+export interface GetCryptoHistoryArgs {
+  coinId: CoinId;
+  timePeriod: TimePeriod;
 }
 
 export interface GetNewsResponse {
@@ -56,6 +88,49 @@ export interface Coin {
   sparkline: string[];
   coinrankingUrl: string;
   "24hVolume": string;
+}
+
+export interface CoinDetail {
+  uuid: string;
+  symbol: string;
+  name: string;
+  description: string;
+  color: string;
+  iconUrl: string;
+  websiteUrl: string;
+  numberOfMarkets: number;
+  numberOfExchanges: number;
+  "24hVolume": string;
+  marketCap: string;
+  price: string;
+  btcPrice: string;
+  priceAt: number;
+  change: string;
+  rank: number;
+  sparkline: string[];
+  coinrankingUrl: string;
+  tier: number;
+  lowVolume: boolean;
+  listedAt: number;
+  allTimeHigh: {
+    price: string;
+    timestamp: number;
+  };
+  supply: {
+    confirmed: boolean;
+    total: string;
+    circulating: string;
+  };
+  links: {
+    name: string;
+    type: string;
+    url: string;
+  }[];
+}
+
+export interface CoinHistory {
+  price: string;
+  timestamp: number;
 }
 
 export interface Stats {
