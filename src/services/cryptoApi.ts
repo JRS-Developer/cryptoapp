24d6037c5f4497656224bd3db2b078e031f88872
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-06-25 12:03:25 -0400

    feat: added env variables for api requests

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -4,11 +4,13 @@ import {
   fetchBaseQuery,
 } from "@reduxjs/toolkit/query/react";
 import type { GetCryptoResponse } from "../types";
+const { VITE_RAPID_API_KEY: API_KEY, VITE_RAPID_API_COINR_HOST: API_HOST } =
+  import.meta.env;
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 const headers = {
-  "X-RapidAPI-Key": "1507a2c8b4msh46eeba1afb45bb0p17841bjsna51bf80112b5",
-  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": API_KEY,
+  "X-RapidAPI-Host": API_HOST,
 };
 
 const createRequest = (url: string): FetchArgs => {
@@ -21,8 +23,8 @@ export const cryptoApi = createApi({
     baseUrl,
   }),
   endpoints: (build) => ({
-    getCryptos: build.query<GetCryptoResponse, void>({
-      query: () => createRequest("/coins"),
+    getCryptos: build.query<GetCryptoResponse, number>({
+      query: (count: number) => createRequest(`/coins`),
     }),
   }),
 });
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_RAPID_API_KEY: string;
+  readonly VITE_RAPID_API_NEWS_HOST: string;
+  readonly VITE_RAPID_API_COINR_HOST: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
