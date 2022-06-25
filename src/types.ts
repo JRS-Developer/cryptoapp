3ff89b9c15f887900d90cb22ff0dafe4fa620017
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-06-25 12:05:27 -0400

    feat: added ts types to getnews enpoint

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,42 @@ export interface GetCryptoResponse {
   };
 }
 
+export interface GetNewsResponse {
+  value: NewArticle[];
+}
+
+export interface GetNewsArgs {
+  count: number;
+  newsCategory: string;
+}
+
+export interface NewArticle {
+  name: string;
+  url: string;
+  image: ArticleImage;
+  provider: ArticleProvider[];
+  description: string;
+  datePublished: string;
+  category?: string;
+}
+
+export interface ArticleImage {
+  thumbnail: {
+    width: number;
+    height: number;
+    contentUrl: string;
+  };
+}
+
+export interface ArticleProvider {
+  name: string;
+  image: {
+    thumbnail: {
+      contentUrl: string;
+    };
+  };
+}
+
 export interface Coin {
   uuid: string;
   symbol: string;
