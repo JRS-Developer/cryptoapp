1c3ec80b6f8231c41bde4c7660139c2570f92435
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-06-25 21:16:04 -0400

    feat: now the navbar is responsive

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,16 @@
 import { Button, Typography, Menu, Avatar } from "antd";
 import type { MenuProps } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useMatch } from "react-router-dom";
 import {
   HomeOutlined,
   MoneyCollectOutlined,
   BulbOutlined,
   FundOutlined,
+  MenuOutlined,
 } from "@ant-design/icons";
 import Logo from "../../assets/images/cryptocurrency.png";
+import { useEffect, useState } from "react";
+import useWindowSize from "../../hooks/useWindowsSize";
 
 type Link = {
   to: string;
@@ -27,11 +30,6 @@ const links: Link[] = [
     icon: <FundOutlined />,
   },
   {
-    to: "/exchanges",
-    title: "Exchanges",
-    icon: <MoneyCollectOutlined />,
-  },
-  {
     to: "/news",
     title: "News",
     icon: <BulbOutlined />,
@@ -39,25 +37,44 @@ const links: Link[] = [
 ];
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(false);
+  const { width } = useWindowSize();
+  const location = useLocation();
+
+  const toggleActiveMenu = () => setActiveMenu((active) => !active);
+  const selectedKey: string =
+    links.find((link) => link.to === location.pathname)?.to || "";
+
+  useEffect(() => {
+    // if the windowsSize is less than 760 then the menu will close automatically
+    if (width !== undefined && width < 760) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [width]);
+
   return (
-    <div className="nav-container">
+    <nav className="nav-container">
       <div className="logo-container">
         <Avatar src={Logo} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
-        {/* <Button>
-
-            </Button> */}
+        <Button className="menu-control-container" onClick={toggleActiveMenu}>
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        {links.map((link, i) => (
-          <Menu.Item key={i} icon={link.icon}>
-            <Link to={link.to}>{link.title}</Link>
-          </Menu.Item>
-        ))}
-      </Menu>
-    </div>
+      {activeMenu && (
+        <Menu theme="dark" selectedKeys={[selectedKey]}>
+          {links.map((link) => (
+            <Menu.Item key={link.to} icon={link.icon}>
+              <Link to={link.to}>{link.title}</Link>
+            </Menu.Item>
+          ))}
+        </Menu>
+      )}
+    </nav>
   );
 };
 
