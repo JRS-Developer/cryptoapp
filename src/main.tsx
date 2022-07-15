9fbf7fd6346305ab44e07661b5eea8efe3404e95
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-07-15 16:04:45 -0400

    chore: prettier

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
-import  { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
